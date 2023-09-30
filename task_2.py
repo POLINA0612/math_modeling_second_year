496f4b48e2bab69e7c50b01de3f64bd90327e4d9
POLINA0612 <114597592+POLINA0612@users.noreply.github.com>
2023-09-30 17:27:14 +0000

    Task_2.py

--- task_2.py
+++ task_2.py
@@ -3,57 +3,78 @@ class Pyramid:
     def __init__(self, max_h):
         self.max_h = max_h
         self.h = max_h
-        self.bricks_count = 0
         self.current_h = 0
+        self.all_bricks = 0
 
-    def add_bricks(self, n):
-        self.bricks_count += n
+    # def add_bricks(self):
+    #      self.all_bricks += b.work_day.a()
 
     def get_height(self):
+        if self.all_bricks % 5 >= 0:
+            self.current_h += 1
+            self.all_bricks //= 5
         print(self.current_h)
-    
+
     def is_done(self):
         return int(self.current_h * 100 / self.max_h)
 
 class Builder:
-    def __init__(self, max_h, n):
+    def __init__(self, max_h,n):
         self.day = 1
         self.bricks = n
         self.my_pyramid = Pyramid(15)
         self.max_h = max_h
         self.h = max_h
+        self.all_bricks = 0
         
     def buy_bricks(self):
-        n = randint(1,5)
-        if self.bricks + n <= 15:
-            self.bricks += n
+        self.bricks += 15 - self.bricks
     def build_pyramid(self, n):
-        n = randint(1,5)
-        if n > self.bricks:
+        if self.all_bricks % 5 > 0 and self.bricks + (self.all_bricks % 5) <= 15:
+            self.bricks += self.all_bricks % 5
+            self.all_bricks == 0
+            return False
+        elif n > self.bricks:
             print('Нет пирамиды')
             self.buy_bricks()
             return False
-        elif self.bricks >= 5 and self.bricks == 0:
+        elif self.bricks >= 15 and self.bricks == 0:
             print('Научись считать от одного до пяти, придурок')
-            return False
-        else:
+        elif self.bricks - (self.all_bricks % 5) <= 15:
             self.bricks -= n
-            self.my_pyramid.add_bricks(n)
+            # self.my_pyramid.add_bricks()
             print('Типо строим')
-            return False
+            return True
     def work_day(self):
         while self.my_pyramid.is_done() != 100:
-            self.day += 1
             a = randint(1, 5)
-            if self.my_pyramid.max_h < self.my_pyramid.h:
+            self.all_bricks += a
+            if a > self.bricks:
+                self.build_pyramid(a)
+                print('День', self.day)
+                print('За день', a, 'положили кирпичей')
+                print('Докупили', 15 - self.bricks - a, 'кирпичей')
+                print('Количество кирпичей', self.bricks)
+                print('Готовность работы', self.my_pyramid.is_done())
+                self.day += 1
+                self.my_pyramid.get_height()
+                print()
+            elif a + (self.all_bricks % 5) <= 5:
+                self.build_pyramid(a)
+                print('День', self.day)
+                print('За день', a, 'положили кирпичей' )
+                print('Количество кирпичей', self.bricks)
+                print('Готовность работы', self.my_pyramid.is_done())
+                self.day += 1
+                self.my_pyramid.get_height()
+                print()
+            elif self.my_pyramid.max_h < self.my_pyramid.current_h:
                 print('Помянем')
-            else:
-                print('День', self.day,
-                    'За день', a, 'кирпичей', 
-                    'Количество кирпичей', self.bricks,
-                    'Готовность работы', self.my_pyramid.is_done())
-            self.build_pyramid(a)
-            print()
+            
+            
+        self.exit()
+    def exit(self):
+        print('Офигеть, вышло')
 
 b = Builder(15,15)
 
