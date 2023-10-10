ea9be1aa52d8e83ddbab4bcbf66d7aa090cd5832
POLINA0612 <114597592+POLINA0612@users.noreply.github.com>
2023-10-10 21:16:38 +0000

    Просто иерунда

--- Mon.py
+++ Mon.py
@@ -3,69 +3,75 @@ class Senior_tomato:
               1: "Цветение",
               2: "Зелёные помидоры",
               3: "Красные помидоры"}
+    
     def __init__(self):
         self.index = 0
         self.state = self.states[0]
+    
     def grow(self):
         if self.state == self.states[0]:
-            self.state == self.states[1]
+            self.state = self.states[1]
         elif self.state == self.states[1]:
-            self.state == self.states[2]
+            self.state = self.states[2]
         elif self.state == self.states[2]:
-            self.state == self.states[3]
+            self.state = self.states[3]
+    
     def is_ripe(self):
-        if self.states == self.states[3]:
+        if self.state == self.states[3]:
             print('Можно собирать урожай')
-            return 1
-        return 0
-
+            return True
+        else:
+            return False
 
 
 class Tomato_bush:
     def __init__(self, n):
         self.n = n
         self.tomatoes = [Senior_tomato() for i in range(self.n)]
-    def grow_all(self, n):
+    
+    def grow_all(self):
         for tomato in self.tomatoes:
             tomato.grow()
+    
     def all_are_ripe(self):
-        k = 0
         for tomato in self.tomatoes:
-           if tomato.is_ripe() == 1:
-                k += 1
-                print('Папой клянусь, всё созрело')
-        if k == len(self.tomatoes):
-            return True
-        return False
+            if not tomato.is_ripe():
+                return False
+        return True
+    
     def give_away_all(self):
-        if self.all_are_ripe() == 1:
+        if self.all_are_ripe():
             self.tomatoes = []
             print('Собрали урожай.')
-
+        else:
+            print('Помидоры еще не созрели.')
+            
 
 class Gardener:
     def __init__(self):
         self.name = 'Грагас'
         self.plant = Tomato_bush(10)
+    
     def work(self):
-        for tomato in self.plant.tomatoes:
-            tomato.grow()
+        self.plant.grow_all()
+    
     def harvest(self):
         if self.plant.all_are_ripe():
             print('Собираем урожай')
+            self.plant.give_away_all()
         else:
             self.knowledge_base()
+    
     def knowledge_base(self):
-        print('Справка по садоводства (для дебилов):')
-        print('1.Выберите подходящее место для выращивания помидоров. Они нуждаются в большом количестве солнечного света.')
-        print('2.Подготовьте почву для поимдоров: разрыхлите почву и удобрите её органическими веществами.')
+        print('Справка по садоводству (для дебилов)')
+        print('1.Выберите подходящее место для выращивания помидоров. Они нуждаются в большом количестве солнечного света')
+        print('2.Подготовьте почву для помидоров: разрыхлите почву и удобрите её органическими веществами.')
         print('3.Следите за поливом. Помидоры нуждаются в регулярном поливе. Но не вздумайте переусердствовать.')
         print('4.Обрезайте растения. Чтобы помидоры росли крупными и здоровыми, необходимо обрезать лишние листья и побеги.')
         print('5.Защитите растения от вредителей и болезней.')
         print('Удачи в выращивании помидоров!')
 
 
-Gardener.knowledge_base(1)
-g = Gardener()
-g.work()
-g.harvest()
+gardener = Gardener()
+gardener.work()
+gardener.harvest()
