0843c1a43e51bb6b1d943cf532876891c47f1e3a
POLINA0612 <114597592+POLINA0612@users.noreply.github.com>
2023-11-01 16:17:46 +0000

    yes

--- Task_1.py
+++ Task_1.py
@@ -1,11 +1,43 @@
+from random import randint 
 class Human:
-    def __init__(self, name, age):
-        self.default_name = name
-        self.default_age = age
+    default_name = input()
+    default_age = input()
+    def __init__(self, name=default_name, age=default_age):
+        self.name = name
+        self.age = age
         self._own_home = 0
         self._money = input()
 
     def info(self):
-        print(default_name)
-        print(default_age)
-        print(_money)
+        print(self.name)
+        print(self.age)
+        print(self._money)
+    
+    def _make_deal(self, house):
+        self._own_home += 1
+        self._money -= house._price
+    
+    def earn_money(self):
+        self._money += randint(100000, 99999999)
+    
+    def buy_house(self, house):
+        if self._money - house._price > 0:
+            self._make_deal()
+    
+
+class House:
+    def __init__(self, area, price):
+        self._area = area
+        self._price = price
+    
+    def final_price(self, price):
+        self.price = price * (1 - 0,35)
+    
+
+class SmallHouse(House):
+    def __init__(self, area, price):
+        super().__init__(area, price)
+        self.area = 40
+
+human = Human()
+
