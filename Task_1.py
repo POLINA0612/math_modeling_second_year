cd088cf9ee66f42998d123cde183bc561e093c6f
POLINA0612 <114597592+POLINA0612@users.noreply.github.com>
2023-11-01 15:24:50 +0000

    ok

--- Task_1.py
+++ Task_1.py
@@ -1,21 +1,11 @@
-class SchoolJournal:
-    def __init__(self, subject, student):
-        self.subject = subject
-        self.student = student
-        self.grade_list = []
-    def grade(self, n):
-        self.grade_list.append(n)
-    def printer(self):
-        print(self.subject, self.student, self.grade_list)
-    def final_grade(self):
-        print(sum(self.grade_list)/len(self.grade_list))
+class Human:
+    def __init__(self, name, age):
+        self.default_name = name
+        self.default_age = age
+        self._own_home = 0
+        self._money = input()
 
-SJ = SchoolJournal('Physics','Matre de Ebale')
-
-from random import randint
-
-for _ in range(15):
-    SJ.grade(randint(2,100))
-
-SJ.printer()
-SJ.final_grade()
+    def info(self):
+        print(default_name)
+        print(default_age)
+        print(_money)
--- create_class.py
+++
@@ -1,7 +0,0 @@
-class Ball:
-    pass
-ball_1 = Ball()
-ball_2 = Ball()
-
-print(help(Ball))
-print(help(ball_1))
